5e65b3bcd6b33645d71b7ab9d61ebfd52e52fb41
Med <mohamed.boucadair@orange.com>
2024-12-15 10:30:23 +0100

    Update yang.mk

--- yang/yang.mk
+++ yang/yang.mk
@@ -24,6 +24,7 @@ pyang-lint: $(STDYANG) $(STDYANGDIR)
 
 yang-lint: $(STDYANG) $(STDYANGDIR)
 	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -p $(STDYANGDIR)/standard/ieee/published/802.1/ $(STDYANG) -i
+        yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -p $(STDYANGDIR)/standard/ieee/published/802.1/ $(STDYANG) $(EXPJSON)
 
 yang-gen-diagram: yang-lint $(TXT)
 
